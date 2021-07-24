1ae08c8732f85ce81d7d4a0f9165eca6ab0e9ec7
tyson <65432197+TYSON-Alii@users.noreply.github.com>
2021-07-24 20:48:54 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,15 +1,13 @@
-﻿#include "XsAsciiRenderEngine.hpp"
+#include "XsAsciiRenderEngine.hpp"
 
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false); // for fast rendering
-	cin.tie(NULL); // for fast rendering
-	XsWindow window(75, 25 /* window scale */);
+	XsWindow window(75, 25);
 	window.name("yarra");
 	window.pos(150);
 	window.scale(75, 25);
-	window.background('#'); /* background char */
+	window.background('#');
 	window.color(XS_WHITE, XS_DARKGREY);
 
 	XsPoint point;
@@ -29,28 +27,6 @@ int main() {
 	rect2.lett('B');
 	rect2.pos = vex2(15, 10);
 
-	XsTexture tex;
-	const char* _tex =
-		"###0###"
-		"#/-|-\\#"
-		"/##|##\\"
-		"##/#\\##"
-		"##|#|##";
-	byte bgcolor = calcolor(XS_WHITE, XS_DARKGREY);
-	byte whiteblue = calcolor(XS_WHITE, XS_BLUE);
-	byte _colortex[]{
-		bgcolor,	bgcolor,	bgcolor,	whiteblue,	bgcolor,	bgcolor,	bgcolor,
-		bgcolor,	whiteblue,	whiteblue,	whiteblue,	whiteblue,	whiteblue,	bgcolor,
-		whiteblue,	bgcolor,	bgcolor,	whiteblue,	bgcolor,	bgcolor,	whiteblue,
-		bgcolor,	bgcolor,	whiteblue,	bgcolor,	whiteblue,	bgcolor,	bgcolor,
-		bgcolor,	bgcolor,	whiteblue,	bgcolor,	whiteblue,	bgcolor,	bgcolor,
-	};
-	tex.load(vex2(7, 5), _tex);
-	tex.loadColor(vex2(7, 5), _colortex);
-	XsMesh mesh;
-	mesh.pos = vex2(30, 7);
-	mesh.tex = tex;
-
 	XsPoint tpoint;
 	vex2 _tpos[]{
 		vex2(0, -2),
@@ -60,11 +36,11 @@ int main() {
 		vex2(-1, 2), vex2(1, 2)
 	};
 	char _tch[]{
-		'0',
+				  '0',
 		'/', '-', '|', '-', '\\',
-		'/','|','\\',
-		'/','\\',
-		'|','|'
+		'/',	  '|',		'\\',
+			   '/',	  '\\',
+			   '|',	  '|'
 	};
 	byte whiteblack = calcolor(XS_WHITE, XS_BLACK);
 	byte whiteyellow = calcolor(XS_WHITE, XS_YELLOW);
@@ -75,24 +51,31 @@ int main() {
 		whiteyellow, whiteyellow,
 		whiteblack, whiteblack
 	};
-	XsBody body;
+	XsBody man;
 	for (size_t i = 0; i < 13; i++) {
 		tpoint.pos = _tpos[i];
 		tpoint.lett(_tch[i]);
 		tpoint.color(_tcolor[i]);
-		body << tpoint;
+		man << tpoint;
 	};
-	body.pos(45, 8);
+	man.pos(45, 8);
+
 	while (true) {
-		// exit button
 		if (XsKeyPressed(XS_KEY_ESC))
 			break;
-		// << for drawing
+		window.clear();
 		window << point;
 		window << rect2;
-		window << mesh;
-		window << body;
-		window.draw(1.f/* fps */);
+		if (XsKeyPressed(XS_KEY_A))
+			man.pos().x -= 1;
+		else if (XsKeyPressed(XS_KEY_D))
+			man.pos().x += 1;
+		if (XsKeyPressed(XS_KEY_W))
+			man.pos().y -= 1;
+		else if (XsKeyPressed(XS_KEY_S))
+			man.pos().y += 1;
+		window << man;
+		window.draw(0.1f);
 	};
 	return 0;
 }
